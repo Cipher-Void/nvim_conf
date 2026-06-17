ec5112b3424ede5c129e0953d2a0d2d8d84e7a4e
BillCipherVoid <billciphervoid@gmail.com>
2026-06-18 00:52:20 +0300

    ref: Sessions
    Переделана кнопка в встречающем меню что бы можно было загрузить сессию
    Перенесены настройки по session из kymaps в obsession

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -50,16 +50,3 @@ vim.keymap.set('n', '[q', ':cprevious<CR>', { noremap = true, silent = true, des
 vim.keymap.set('n', '<leader>qq', ':cclose<CR>', { noremap = true, silent = true, desc = "Close Quickfix" })  
 vim.keymap.set('n', '<leader>qr', ':silent! cbuffer<CR>', { noremap = true, silent = true, desc = "update Quickfix" })  
 
--- Если Session.vim в текущей рабочей директории
-local function load_session()
-end
-
-vim.api.nvim_create_user_command("LoadSession", function()
-  local path = vim.fn.expand("Session.vim")
-  if vim.fn.filereadable(path) == 1 then
-    vim.cmd("source " .. vim.fn.fnameescape(path))
-  else
-    vim.notify("Session.vim not found: " .. path, vim.log.levels.WARN)
-  end
-end, {})
-vim.api.nvim_create_user_command("LS", function() vim.cmd("LoadSession") end, {})
--- lua/plugins/obsession.lua
+++ lua/plugins/obsession.lua
@@ -1,3 +1,17 @@
+-- Если Session.vim в текущей рабочей директории
+local function load_session()
+end
+
+vim.api.nvim_create_user_command("LoadSession", function()
+  local path = vim.fn.expand("Session.vim")  -- Файл в котором храниться сессия vim
+  if vim.fn.filereadable(path) == 1 then
+    vim.cmd("source " .. vim.fn.fnameescape(path))
+  else
+    vim.notify("Session.vim not found: " .. path, vim.log.levels.WARN)
+  end
+end, {})
+vim.api.nvim_create_user_command("LS", function() vim.cmd("LoadSession") end, {})
+
 return {
     {
         "tpope/vim-obsession",
