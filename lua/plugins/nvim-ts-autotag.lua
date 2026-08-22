9114539465d2cbc9f4a1f259c0c3bf654f7fdfb8
CipherVoid <billciphervoid@gmail.com>
2026-08-22 13:52:43 +0300

    ts-autotag lazyload ft filytype

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    event = function()
+      return {}
+    end,
+    ft = { "html", "xml", "jsx", "tsx", "vue", "svelte", "astro", "javascriptreact", "typescriptreact" },
+  },
+}
